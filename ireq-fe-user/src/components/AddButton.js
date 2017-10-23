fdde1f13df00640d0ba96fb9504a678ae61f5350
Dpartipilo <Dpartipilo@hotmail.com>
2017-10-23 15:37:54 +0100

    update [AddButton]

--- ireq-fe-user/src/components/AddButton.js
+++ ireq-fe-user/src/components/AddButton.js
@@ -1,1 +1,16 @@
 import React, { Component } from 'react';
+
+
+class AddButton extends Component {
+
+  render() {
+    return (
+      <div>
+        <span>+</span>
+      </div>
+    );
+  }
+}
+
+
+export default AddButton;
