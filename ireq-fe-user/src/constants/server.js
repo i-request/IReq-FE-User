86effd76447a19952d507c2a7a4a0c16871d1412
d-king27 <kingd8883@gmail.com>
2017-11-02 13:51:12 +0000

    changed payment server

--- ireq-fe-user/src/constants/server.js
+++ ireq-fe-user/src/constants/server.js
@@ -1,5 +1,5 @@
 const PAYMENT_SERVER_URL = process.env.NODE_ENV === 'dev'
 ? 'https://irequest-be.herokuapp.com/payment'
-: 'https://irequest-be.herokuapp.com//payment';
+: 'https://irequest-be.herokuapp.com/payment';
 
 export default PAYMENT_SERVER_URL;
