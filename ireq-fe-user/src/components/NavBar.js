1280e54a3f4cd75a4fdd7bb2302d11c07da9ea34
Jonny Ward <JonnyWard@Macs-MacBook-Pro.local>
2017-11-02 14:13:11 +0000

    updated user

--- ireq-fe-user/src/components/NavBar.js
+++ ireq-fe-user/src/components/NavBar.js
@@ -7,7 +7,7 @@ class NavBar extends Component {
       <div>
         <nav className="navbar navbar-expand-lg navbar-light bg-light main-nav">
           <div className="navbar-brand">
-            
+
           </div>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
@@ -20,9 +20,6 @@ class NavBar extends Component {
                 <NavLink to="/" className="link-anchor">Home
                   <span className="sr-only">(current)</span>
                 </NavLink>
-              </li>
-              <li className="nav-li">
-                <NavLink to='/about' className="link-anchor">About</NavLink>
               </li>
               <li className="nav-li">
                 <NavLink to='/menu' className="link-anchor">Menu</NavLink>
